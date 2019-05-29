642620ddf73457013b345b01095dfdeb05db0689
timba <gabrieltimba@hotmail.com>
2019-05-29 07:03:46 -0700

    Update autenticacao.php

--- Aplicacao/autenticacao.php
+++ Aplicacao/autenticacao.php
@@ -3,13 +3,9 @@
     if(isset($_POST['entrar'])){
         inicarSessao();
     }
-        
-    if(isset($_POST['sair'])){
+   
+    if(isset($_GET['sair'])){
         terminarSessao();
-    }
-
-    if(isset($_POST['registar'])){
-        registar();
     }
 
     function inicarSessao(){
@@ -36,22 +32,6 @@
         }
     }
 
-    function registar(){
-        include('bd.php');
-        
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $pass = $_POST['password'];
-        
-        $query = "INSERT INTO users (nome , email, password) 
-        VALUES ('$nome' , '$email', '$pass')";
-
-        if (mysqli_query($conexao, $query) === TRUE) {
-            header("Location:login.php");
-        } else {
-        echo "Ha problemas".mysqli_error($conexao);
-        }
-    }
 
     function terminarSessao(){
         session_start();
