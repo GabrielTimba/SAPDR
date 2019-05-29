505a94120958699207e82f33ee0ab072708d88a5
jossias <mupandzaj@gmail.com>
2019-05-29 16:01:23 +0200

    cadastro com preparedstatment - atualisado

--- Aplicacao/model/doencaDAO.php
+++ Aplicacao/model/doencaDAO.php
@@ -1,36 +1,8 @@
 <?php
     
-    inserir2();
+    inserir();
     
     function inserir() {
-        include('../bd.php');
-
-        $nome= $_POST['nome'];
-        $tipo= $_POST['tipo'];
-        $causas= $_POST['causas'];
-        $sintomas= $_POST['sintomas'];
-        $tratamentos= $_POST['tratamentos'];
-        $prevencao= $_POST['prevencao'];
-
-        $insere = "
-            IF (SELECT idTipo FROM tipod t WHERE t.nome='$tipo') IS NOT NULL THEN
-                INSERT INTO doenca (idDoenca, nome, prevencao, causa, tratamento, sintoma, idTipo) VALUES ('NULL','$nome','$prevencao','$causas','$tratamentos','$sintomas',(SELECT idTipo FROM tipod t WHERE t.nome='$tipo'));
-            ELSE
-                INSERT INTO tipod (idTipo,nome) VALUES('NULL','$tipo');
-                INSERT INTO doenca (idDoenca, nome, prevencao, causa, tratamento, sintoma, idTipo) VALUES ('NULL','$nome','$prevencao','$causas','$tratamentos','$sintomas',(SELECT idTipo FROM tipod t WHERE t.nome='$tipo')); 
-            END IF;
-            " ;
-        
-        if (mysqli_query($conexao, $insere)) {
-            header('Location:../admin/registar-doencas.php');
-        } else {
-            echo "Erro na gravacao de dados: $insere <br>" . mysqli_error($conexao);
-        }
-
-        mysqli_close($conexao);
-    }
-
-    function inserir2() {
         include('../bd.php');
 
         $nome= $_POST['nome'];
@@ -72,13 +44,6 @@
         if(!($stmt->execute())){
             echo " Execucao falhou: (".$stmt->errno.")".$stmt->error;
         }
-        
-
-       /* if (mysqli_query($conexao, $insere)) {
-            header('Location:../admin/registar-doencas.php');
-        } else {
-            echo "Erro na gravacao de dados: $insere <br>" . mysqli_error($conexao);
-        }*/
 
         $stmt->close();
         $conexao->close();
