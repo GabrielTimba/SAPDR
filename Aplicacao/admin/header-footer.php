a29cc1fef93750d114369c4909c7d4cc5412e66a
Folege <follegelricardo@gmail.com>
2019-06-02 15:12:09 +0200

    Update header-footer.php

--- Aplicacao/admin/header-footer.php
+++ Aplicacao/admin/header-footer.php
@@ -76,12 +76,12 @@
                         </ul>
                     </li>
                     <hr class="linha">
-                    <li><a href="#"><i class="fas fa-h-square mr-2"></i>Instituições e Associações</a></li>
+                    <li><a href="lista-de-instituicoes.php"><i class="fas fa-h-square mr-2"></i>Instituições e Associações</a></li>
                     <hr class="linha">
                     <li>
                         <a class="dropdown-toggle" data-toggle="collapse" aria-expanded="false" href="#menu-mensagens"><i class="fa fa-envelope mr-2"></i> Mensagens</a>
                         <ul class="collapse list-unstyled menu-dropdown" id="menu-mensagens">
-                            <li><a href="#"><i class="fas fa-angle-double-right mr-2"></i>Pedidos de Apoio</a></li>
+                            <li><a href="lista-de-pedidos.php"><i class="fas fa-angle-double-right mr-2"></i>Pedidos de Apoio</a></li>
                             <li><a href="mensagens.php"><i class="fas fa-angle-double-right mr-2"></i>Mensagens de Suporte</a></li>
                         </ul>
                     </li>
