d509ff69515fe9e156ce67b07db0156517c39210
Folege <follegelricardo@gmail.com>
2019-06-03 11:39:35 +0200

    Update documentosDAO.php

--- Aplicacao/model/documentosDAO.php
+++ Aplicacao/model/documentosDAO.php
@@ -21,12 +21,14 @@
                 echo'
                     <tr>
                         <td class="text-center">
-                            <input type="checkbox" id="cb-'.$cb.'" value="'.$linha['idPost'].'">
+                            <input type="checkbox" id="cb-'.$cb.'" value="'.$linha['idDoc'].'">
                         </td>
                         <td>'.$linha['titulo'].'</td>
-                        <td>'.$linha['data'].'</td>
+                        <td>'.$linha['descricao'].'</td>
+                        <td>'.$linha['arquivo'].'</td>
+                       
                         <td> 
-                            <a class="btn cor-verde"  href="../model/profissionalDAO.php?id='.$linha['idPost'].'">
+                            <a class="btn cor-verde"  href="../model/profissionalDAO.php?id='.$linha['idDoc'].'">
                                 Ler Mais
                             </a>
                         </td>
@@ -42,5 +44,45 @@
         $stmt->close();
         fechaConexao($conexao);
     }
+
+    define('INSERIR',' call inserirDocumento(?,?,?,?,?)');
+
+    if(isset($_POST['submeter'])){
+        inserirDoc();
+    }
+    
+    function inserirDoc(){
+        $con=getConexao();
+
+        // prepared statment 
+        $stmt = $con->prepare(INSERIR);
+        if(!$stmt) {
+            echo"Preparo da Insercao Falhou: (".$con->errno.") ".$conexao->error;
+        } 
+
+        if(!($stmt->bind_param('ssbss',$titulo,$descricao,$arquivo,$user,$senha))) {
+            echo " Parâmetros de ligação falhados: (".$stmt->errno.")".$stmt->error;
+        }
+
+        $titulo= $_POST['titulo'];
+        $descricao= $_POST['descricao'];
+        $arquivo= $_POST['arquivo'];
+
+        session_start();
+	    if(isset($_SESSION['nomeAdmin'])){
+            $user=$_SESSION['nomeAdmin'];
+            $senha=$_SESSION['senhaAdmin']; 
+        }
+        
+        //executando
+        if(!($stmt->execute())){
+            echo " Execucao falhou: (".$stmt->errno.")".$stmt->error;
+        }
+
+        fechaConexao($con);
+
+       header('Location:../admin/index.php');
+    }
+
 
 ?>
