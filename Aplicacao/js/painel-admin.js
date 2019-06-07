40d7cebdf8dc110394b1504942f9e6644d2d9abc
timba <gabrieltimba@hotmail.com>
2019-06-07 05:28:10 -0700

    Update painel-admin.js

--- Aplicacao/js/painel-admin.js
+++ Aplicacao/js/painel-admin.js
@@ -9,24 +9,24 @@ $(document).ready(function () {
 requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=1','v1');
 setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=1","v1");},5000);
 
-requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=1','v2');
+requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=2','v2');
 setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=2","v2");},5000);
 
-requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=1','v3');
+requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=3','v3');
 setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=3","v3");},5000);
 
 requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=4','v4');
-setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=1","v4");},5000);
+setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=4","v4");},5000);
 
 requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=5','v5');
-setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=1","v5");},5000);
+setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=5","v5");},5000);
 
 requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=6','v6');
 setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=6","v6");},5000);
 
-requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=6','v7');
+requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=7','v7');
 setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=7","v7");},5000);
 
-requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=6','v8');
-setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=6","v8");},5000);
+requisitarAjax('GET','../model/estatisticaDAO.php?accao=true&&n=8','v8');
+setInterval(function(){requisitarAjax("GET","../model/estatisticaDAO.php?accao=true&&n=8","v8");},5000);
 
