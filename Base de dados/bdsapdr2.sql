4ce7ff4ebe07975c3b7f0660187a4e38ff45f3c1
timba <gabrieltimba@hotmail.com>
2019-06-10 05:15:06 -0700

    bd

--- Base de dados/bdsapdr2.sql
+++ Base de dados/bdsapdr2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 02-Jun-2019 às 14:07
+-- Generation Time: 07-Jun-2019 às 11:29
 -- Versão do servidor: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -39,6 +39,27 @@ BEGIN
     
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizarInstituicao` (IN `_nome` VARCHAR(50), IN `_tipo` VARCHAR(50), IN `_provincia` VARCHAR(50), IN `_distrito` VARCHAR(50), IN `_bairro` VARCHAR(50), IN `_rua` VARCHAR(50))  NO SQL
+BEGIN
+	DECLARE codEndereco SMALLINT(10);
+    	DECLARE codTipo SMALLINT(10);
+    
+    
+    SELECT idEndereco INTO codEndereco FROM endereco WHERE provincia = _provincia and distrito = _distrito and bairro = _bairro and rua = _rua;
+    
+     If (codEndereco is null )then 
+    	INSERT INTO  endereco VALUES (null,_provincia,_distrito,_bairro,_rua);
+        SELECT idEndereco INTO codEndereco FROM endereco WHERE provincia = _provincia and distrito = _distrito and bairro = _bairro and rua = _rua;
+    end if;
+    
+    SELECT idTipoI INTO codTipo FROM tipoi WHERE nome = _tipo;
+    
+    IF codTipo IS NOT NULL THEN
+        UPDATE instituicao SET nome = _nome, idEndereco = codEndereco, idTipoI = codTipo WHERE idInstituicao = _id;
+    END IF;
+    
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `apagarDoenca` (IN `_id` INT(6))  BEGIN
 	DECLARE cod SMALLINT(6);
 	SELECT idDoenca INTO cod FROM doenca WHERE idDoenca = _id;
@@ -47,10 +68,10 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `apagarDoenca` (IN `_id` INT(6))  BE
     END IF;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `inseriDoente` (IN `_nome` VARCHAR(50), IN `_apelido` VARCHAR(50), IN `_data` DATE, IN `_genero` ENUM('Masculino','Femenino','',''), IN `_provincia` VARCHAR(50), IN `_distrito` VARCHAR(50), IN `_bairro` VARCHAR(50), IN `_rua` VARCHAR(50), IN `_email` VARCHAR(50), IN `_tellefone` INT, IN `_doenca` VARCHAR(50), IN `_idInst` SMALLINT(10), IN `_user` VARCHAR(50), IN `_senha` VARCHAR(50))  NO SQL
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inseriDoente` (IN `_nome` VARCHAR(50), IN `_apelido` VARCHAR(50), IN `_data` DATE, IN `_genero` ENUM('Masculino','Femenino','',''), IN `_provincia` VARCHAR(50), IN `_distrito` VARCHAR(50), IN `_bairro` VARCHAR(50), IN `_rua` VARCHAR(50), IN `_email` VARCHAR(50), IN `_tellefone` INT, IN `_doenca` SMALLINT(10), IN `_idInst` SMALLINT(10), IN `_user` VARCHAR(50), IN `_senha` VARCHAR(50))  NO SQL
 BEGIN
 	Declare codEndereco smallint(10);
-    Declare codDoenca smallint(10);
+    Declare codDoente smallint(10);
    
     Select idEndereco into codEndereco from endereco 
     where bairro = _bairro AND rua=_rua AND provincia=_provincia and distrito=_distrito;
@@ -61,10 +82,10 @@ BEGIN
         where bairro = _bairro AND rua=_rua AND provincia=_provincia and distrito=_distrito;
     end if;
 	    
-    
+   
     INSERT INTO doente VALUES 	(null,_nome,_apelido,_data,genero,_idInst,codEndereco,_user,md5(_senha));
-    select idDoenca INTO codDoenca FROM doenca ORDER by idDoenca DESC LIMIT 1;
-    INSERT INTO doenca_doente VALUES (iddoenca,idDoente);
+    select idDoente INTO codDoente FROM doente ORDER by idDoente DESC LIMIT 1;
+    INSERT INTO doenca_doente VALUES (_doenca,codDoente);
     
     
 END$$
@@ -79,6 +100,16 @@ BEGIN
     
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inserirDocumento` (IN `_titulo` VARCHAR(50), IN `_descricao` VARCHAR(100), IN `_arquivo` BLOB, IN `_user` VARCHAR(50), IN `_senha` VARCHAR(50))  NO SQL
+BEGIN
+DECLARE codProf SMALLINT(10);
+
+	SELECT idProf INTO codProf from profissional WHERE UserName=_user and senha=md5(_senha);
+    
+    INSERT INTO documento VALUES (null,_titulo,_descricao,_arquivo,codProf,now());
+    
+ END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `inserirDoenca` (IN `_nome` VARCHAR(50), IN `_tipo` VARCHAR(30), IN `_causas` TEXT, IN `_sintomas` TEXT, IN `_tratamentos` TEXT, IN `_prevencao` TEXT)  BEGIN
 	DECLARE tipo VARCHAR(40);
     
@@ -92,10 +123,42 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `inserirDoenca` (IN `_nome` VARCHAR(
     END IF;
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inserirInstituicao` (IN `_nome` VARCHAR(50), IN `_tipo` VARCHAR(50), IN `_provincia` VARCHAR(50), IN `_distrito` VARCHAR(50), IN `_bairro` VARCHAR(50), IN `_rua` VARCHAR(50))  NO SQL
+BEGIN
+	Declare codEndereco smallint(10);
+    Declare codTipo smallint(10);
+   
+    Select idEndereco into codEndereco from endereco 
+    where bairro = _bairro AND rua=_rua AND provincia=_provincia and distrito=_distrito;
+    
+    If (codEndereco is null) then 
+    	INSERT INTO  endereco VALUES (null,_provincia,_distrito,_bairro,_rua);
+        Select idEndereco into codEndereco from endereco 
+        where bairro = _bairro AND rua=_rua AND provincia=_provincia and distrito=_distrito;
+    end if;
+    
+    Select idTipoI into codTipo from tipoi 
+    where nome = _tipo ;
+    
+    If (codTipo is null) then 
+    	INSERT INTO tipoi VALUES (null,_tipo);
+         Select idTipoI into codTipo from tipoi where nome = _tipo ;
+    end if;
+	    
+    INSERT INTO instituicao VALUES 	(null,_nome,codEndereco,codTipo);
+    
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `lerDoencaID` (IN `_id` INT(4))  BEGIN
 	SELECT d.idDoenca as id,d.nome, t.nome as tipo, d.causa, d.sintoma, 			d.tratamento, d.prevencao
     FROM doenca d, tipod t
     WHERE d.idTipo = t.idTipo AND d.idDoenca = _id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lerDoencaNome` (IN `_nome` VARCHAR(40))  BEGIN
+	SELECT d.idDoenca as id, d.nome, t.nome as tipo, d.causa, d.sintoma, 			d.tratamento, d.prevencao
+    FROM doenca d, tipod t
+    WHERE d.idTipo = t.idTipo AND d.nome LIKE _nome;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `lerDoencas` (IN `_n` TINYINT(1))  BEGIN
@@ -112,12 +175,100 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `lerDoencas` (IN `_n` TINYINT(1))  B
     END IF;
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lerInstituicaoID` (IN `_id` INT(10))  NO SQL
+SELECT i.nome as nome,t.nome as tipo,provincia,distrito,bairro,rua from instituicao i, tipoi t, endereco e where _id = i.idInstituicao and i.idEndereco = e.idEndereco AND i.idInstituicao = t.idTipoI$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lerPosts` (IN `_tipo` VARCHAR(20), IN `_n` INT(1))  BEGIN
+	DECLARE cod SMALLINT(4);
+    SELECT idTipoP into cod FROM tipop WHERE nome = _tipo; 
+    IF (_n = 1) THEN
+    	SELECT idPost, autor, titulo FROM post WHERE idTipoP = cod;
+    ELSEIF (_n = 2) THEN 
+    	SELECT idPost,autor, msg,titulo, dataHora FROM post WHERE idTipoP = cod ORDER BY idPost DESC;
+    ELSEIF (_n = 3) THEN
+    	SELECT idPost, autor, msg,titulo, dataHora FROM post WHERE idTipoP = cod ORDER BY idPost DESC LIMIT 1;
+    END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_associacoes` ()  NO SQL
+SELECT idAssociacao,nome,descricao,bairro FROM associacao a,endereco e WHERE e.idEndereco = a.idEndereco$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_de_documentos` ()  NO SQL
+SELECT idDoc,titulo,descricao,arquivo from documento$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_de_doentes` ()  NO SQL
+SELECT d.idDoente,dr.nome as doenca,d.nome,d.apelido,c.email,c.telefone FROM doente d,contacto c,doenca_doente dd,doenca dr WHERE dr.idDoenca = dd.idDoenca and dd.idDoente=d.idDoente$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_de_instituicoes` ()  NO SQL
+SELECT idInstituicao as idInst,i.nome,t.nome as tipo,bairro FROM instituicao i,tipoi t,endereco e where i.idEndereco = e.idEndereco and i.idTipoi = t.idTipoi$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_de_Mensagens` ()  NO SQL
 SELECT idMensagem ,assunto,email,msg FROM mensagem  WHERE estado='Pendente'$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_de_pedidos` (IN `_estado` VARCHAR(50))  NO SQL
+SELECT * from apoio where _estado = estado$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_de_profissionais` ()  NO SQL
 select d.idProf,d.nome,c.email,i.nome as unidade from profissional d,contacto c,instituicao i
 where c.idContacto = d.idContacto and i.idInstituicao = d.idInstituicao$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_farmacias` ()  NO SQL
+SELECT idInstituicao,i.nome,t.nome as tipo,bairro FROM instituicao i,tipoi t,endereco e where i.idEndereco = e.idEndereco and i.idTipoi = t.idTipoi and t.nome="Farmacia"$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lista_hospital` ()  NO SQL
+SELECT idInstituicao,i.nome,t.nome as tipo,bairro FROM instituicao i,tipoi t,endereco e where i.idEndereco = e.idEndereco and i.idTipoi = t.idTipoi and t.nome = "Hospital"$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `publicarPed` (IN `_id` INT)  NO SQL
+UPDATE apoio SET estado = "Publicado" WHERE idapoio = _id$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `estatistica` (`tipo` SMALLINT(2)) RETURNS INT(11) BEGIN
+	declare res int(10);
+    /* Buscar numero de doentes*/
+	if(tipo=1) then 
+		select count(idDoente) as resultado into res from doente ;
+	end if;
+    
+    /* Buscar numero de doencas raras*/
+    if(tipo=2) then 
+		select count(iddoenca) into res from doenca;
+	end if;
+    
+    /*buscar o numero de profissionais*/
+    if(tipo=3) then 
+		select count(idProf) into res from profissional;
+	end if;
+    
+    /*buscar o numero de pedidos de apoio pendentes*/
+    if(tipo=4) then 
+		select count(idapoio) into res from apoio where estado="Pendente";
+	end if;
+    
+    /*buscar o numero de mensagenss*/
+    if(tipo=5) then 
+		select count(idmensagem) into res from mensagem ;
+	end if;
+    
+    /*buscar o numero de documentos*/
+    if(tipo=6) then 
+		select count(idDoc) into res from documento;
+	end if;
+    
+    /*buscar o numero de artigos publicados*/
+    if(tipo=7) then 
+		select count(idpost) into res from post where
+        idTipoP=(select idTipoP from tipop where nome="Artigo");
+	end if;
+    
+    /*buscar o numero de campanhas publicados*/
+    if(tipo=8) then 
+		select count(idpost) into res from post where
+        idTipoP=(select idTipoP from tipop where nome="Campanha");
+	end if; 
+RETURN res;
+END$$
 
 DELIMITER ;
 
@@ -130,11 +281,19 @@ DELIMITER ;
 CREATE TABLE `apoio` (
   `idapoio` smallint(10) NOT NULL,
   `pedido` varchar(50) NOT NULL,
-  `telefone` smallint(9) NOT NULL,
+  `telefone` int(20) NOT NULL,
   `msg` text NOT NULL,
   `benefeciario` varchar(50) NOT NULL,
-  `idDoente` smallint(10) NOT NULL
+  `idDoente` smallint(10) NOT NULL,
+  `estado` enum('Pendente','Publicado','','') NOT NULL DEFAULT 'Pendente'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `apoio`
+--
+
+INSERT INTO `apoio` (`idapoio`, `pedido`, `telefone`, `msg`, `benefeciario`, `idDoente`, `estado`) VALUES
+(1, 'wwwww', 820000000, '', 'timba', 3, 'Pendente');
 
 -- --------------------------------------------------------
 
@@ -158,7 +317,7 @@ CREATE TABLE `associacao` (
 CREATE TABLE `contacto` (
   `idContacto` smallint(10) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `telefone` int(10) NOT NULL
+  `telefone` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -177,7 +336,7 @@ INSERT INTO `contacto` (`idContacto`, `email`, `telefone`) VALUES
 CREATE TABLE `documento` (
   `idDoc` smallint(10) NOT NULL,
   `titulo` varchar(50) NOT NULL,
-  `descricao` int(11) NOT NULL,
+  `descricao` text NOT NULL,
   `arquivo` blob NOT NULL,
   `idProf` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -203,7 +362,9 @@ CREATE TABLE `doenca` (
 --
 
 INSERT INTO `doenca` (`idDoenca`, `nome`, `prevencao`, `causa`, `tratamento`, `sintoma`, `idTipo`) VALUES
-(1, 'aaaa', 'SCc', 'sss', 'add', 'ddd', 1);
+(1, 'aaaa', 'SCc', 'sss', 'add', 'ddd', 1),
+(2, 'bbbbbbbbb', 'ttttttttttttt', 'ooooooooooooo', 'pppppppppppppppp', 'jjjjjjjjjjjjjjjj', 1),
+(3, 'eeeeeeeeee', 'fff', 'fff', 'vvvv', 'gggg', 1);
 
 -- --------------------------------------------------------
 
@@ -215,6 +376,19 @@ CREATE TABLE `doenca_doente` (
   `idDoenca` smallint(10) NOT NULL,
   `idDoente` smallint(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `doenca_doente`
+--
+
+INSERT INTO `doenca_doente` (`idDoenca`, `idDoente`) VALUES
+(1, 14),
+(1, 15),
+(1, 16),
+(1, 17),
+(1, 18),
+(2, 12),
+(2, 13);
 
 -- --------------------------------------------------------
 
@@ -241,7 +415,19 @@ CREATE TABLE `doente` (
 INSERT INTO `doente` (`idDoente`, `nome`, `apelido`, `dataNasc`, `genero`, `idInstituicao`, `idEndereco`, `UserName`, `senha`) VALUES
 (2, 'Maria', 'Cossa', '2017-12-06', '', 1, 1, 'cossa', '9d1136878d04238b7b47b236101ca4d4'),
 (3, 'Mazivila', 'Eurico', '0000-00-00', '', 1, 2, 'timba', '3f767f568ac379661ccd599bd40b9859'),
-(5, 'Visitante', '', '0000-00-00', '', 2, 1, 'Visitante', 'a61c72715246b2b92b90a51e06118b05');
+(5, 'Visitante', '', '0000-00-00', '', 2, 1, 'Visitante', 'a61c72715246b2b92b90a51e06118b05'),
+(6, 'fff', 'ggg', '0000-00-00', 'Masculino', 1, 3, 'timba', '81dc9bdb52d04dc20036dbd8313ed055'),
+(7, 'fff', 'ggg', '0000-00-00', 'Masculino', 1, 3, 'timba', '81dc9bdb52d04dc20036dbd8313ed055'),
+(8, 'fff', 'ggg', '0000-00-00', 'Masculino', 1, 3, 'timba', '81dc9bdb52d04dc20036dbd8313ed055'),
+(9, 'fff', 'ggg', '0000-00-00', 'Masculino', 1, 3, 'timba', '81dc9bdb52d04dc20036dbd8313ed055'),
+(11, 'fff', 'ggg', '0000-00-00', 'Masculino', 1, 3, 'timba', 'd93591bdf7860e1e4ee2fca799911215'),
+(12, 'fff', 'ggg', '0000-00-00', 'Masculino', 1, 3, 'timba', '3b712de48137572f3849aabd5666a4e3'),
+(13, 'fff', 'ggg', '0000-00-00', 'Masculino', 1, 3, 'timba', '310dcbbf4cce62f762a2aaa148d556bd'),
+(14, 'Gabriel', 'Timba', '0000-00-00', 'Masculino', 1, 4, 'timba', '01cfcd4f6b8770febfb40cb906715822'),
+(15, 'Folege', 'Timba', '0000-00-00', 'Masculino', 1, 5, 'dddddddd', 'c12e01f2a13ff5587e1e9e4aedb8242d'),
+(16, 'Gotine', 'Masca', '0000-00-00', 'Masculino', 1, 6, 'timba', '81dc9bdb52d04dc20036dbd8313ed055'),
+(17, 'sss', 'fff', '0000-00-00', 'Masculino', 1, 7, 'sss', 'caf1a3dfb505ffed0d024130f58c5cfa'),
+(18, 'Salmento', 'sss', '0000-00-00', 'Masculino', 1, 8, 'ffff', '248e844336797ec98478f85e7626de4a');
 
 -- --------------------------------------------------------
 
@@ -299,7 +485,13 @@ CREATE TABLE `endereco` (
 
 INSERT INTO `endereco` (`idEndereco`, `provincia`, `distrito`, `bairro`, `rua`) VALUES
 (1, 'Maputo', 'Kamubukwane', '25 de junho', ''),
-(2, 'Gaza', 'a', 'c', '2e');
+(2, 'Gaza', 'a', 'c', '2e'),
+(3, '1', 'Maputo', 'hhh', 'g'),
+(4, '1', 'Dondo', '25 de junho', '2'),
+(5, '1', 'Chibuto', 'te', '4'),
+(6, '2', 'Bilene', 'fff', '2'),
+(7, '1', 'KaMpfumo ', 'ddd', 'fa'),
+(8, '1', 'KaMpfumo ', 'w2', '1');
 
 -- --------------------------------------------------------
 
@@ -349,7 +541,10 @@ INSERT INTO `mensagem` (`idmensagem`, `assunto`, `email`, `msg`, `idDoente`, `Es
 (5, 'ss', 'DD@S', 'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj', 2, 'Pendente'),
 (6, 'ss', 'DD@S', 'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj', 2, 'Respondido'),
 (7, 'ss', 'DD@S', 'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj', 2, 'Pendente'),
-(8, 'fefe', 'ff@f', 'aaaaaaaaaaaa', 2, 'Pendente');
+(8, 'fefe', 'ff@f', 'aaaaaaaaaaaa', 2, 'Pendente'),
+(9, 'folege', 'ricaro@gmail.com', 'o Folege afirma que as mensagens nao sao persistidas na base de dados', 5, 'Pendente'),
+(10, 'ddd', 'ggg@dd', 'fff', 5, 'Pendente'),
+(11, 'Neima', 'ddd@s', 'hvehgcvgcvgd', 5, 'Pendente');
 
 -- --------------------------------------------------------
 
@@ -475,6 +670,14 @@ CREATE TABLE `tipop` (
   `idTipoP` smallint(10) NOT NULL,
   `nome` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tipop`
+--
+
+INSERT INTO `tipop` (`idTipoP`, `nome`) VALUES
+(1, 'Artigo'),
+(2, 'Campanha');
 
 --
 -- Indexes for dumped tables
@@ -634,7 +837,7 @@ ALTER TABLE `tipop`
 -- AUTO_INCREMENT for table `apoio`
 --
 ALTER TABLE `apoio`
-  MODIFY `idapoio` smallint(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idapoio` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `associacao`
@@ -658,19 +861,19 @@ ALTER TABLE `documento`
 -- AUTO_INCREMENT for table `doenca`
 --
 ALTER TABLE `doenca`
-  MODIFY `idDoenca` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idDoenca` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `doente`
 --
 ALTER TABLE `doente`
-  MODIFY `idDoente` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idDoente` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `endereco`
 --
 ALTER TABLE `endereco`
-  MODIFY `idEndereco` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idEndereco` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `instituicao`
@@ -682,7 +885,7 @@ ALTER TABLE `instituicao`
 -- AUTO_INCREMENT for table `mensagem`
 --
 ALTER TABLE `mensagem`
-  MODIFY `idmensagem` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idmensagem` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `post`
@@ -718,7 +921,7 @@ ALTER TABLE `tipoi`
 -- AUTO_INCREMENT for table `tipop`
 --
 ALTER TABLE `tipop`
-  MODIFY `idTipoP` smallint(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTipoP` smallint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
